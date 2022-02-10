da20a4bd110097ead2a9b1561353903e8560fb82
AlisherDe <alisher.qurbonov@bk.ru>
2022-02-10 14:06:12 +0500

    alt

--- ViewModels/RegisterViewModel.cs
+++ ViewModels/RegisterViewModel.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace queue.ViewModels;
 
 public class RegisterViewModel
 {
     public Guid Id { get; set; }
+
+    [Required(ErrorMessage = "To'liq ism-sharfini kiritish shart!")]
+    [Display(Name = "Ism-sharf")]
+    
     public string Fullname { get; set; }
     public string Phone { get; set; }
     public DateTimeOffset CreatedAt { get; set; }
